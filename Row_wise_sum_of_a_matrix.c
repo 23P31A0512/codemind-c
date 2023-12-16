18d0d495f43c43eeec1f0e87d2bfce34ff0f7cf9
23P31A0512 <147791827+23P31A0512@users.noreply.github.com>
2023-12-16 11:43:22 +0530

    #include<stdio.h> #include<limits.h> int main() {     int r,c;     scanf("%d%d",&r,&c);     int arr[r][c],i,j,rowsum=0;     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<r;i++)     {         int rowsum=0;         for(j=0;j<c;j++)         {             rowsum=rowsum+arr[i][j];         }         printf("%d ",rowsum);     } }

---
+++ Row_wise_sum_of_a_matrix.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int arr[r][c],i,j,rowsum=0;
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        int rowsum=0;
+        for(j=0;j<c;j++)
+        {
+            rowsum=rowsum+arr[i][j];
+        }
+        printf("%d ",rowsum);
+    }
+}
