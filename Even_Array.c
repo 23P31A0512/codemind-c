b94931ef1d050c2ce6deb74e9d1c2327cbe06042
23P31A0512 <147791827+23P31A0512@users.noreply.github.com>
2023-12-16 11:06:07 +0530

    #include<stdio.h> #include<limits.h> int main() {   int i,n,c=0;   scanf("%d",&n);   int arr[n];   for(i=0;i<n;i++)   {       scanf("%d",&arr[i]);   }   for(i=0;i<n;i++)   {       if(arr[i]%2==0 || arr[i]==0)       {       c++;       }   }   if(c==n)   {       printf("True");   }   else   {       printf("False");   } }

--- Even_Array.c
+++ Even_Array.c
@@ -2,26 +2,26 @@
 #include<limits.h>
 int main()
 {
-    int n,i,c=0;
-    scanf("%d",&n);
-    int arr[n];
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        if(arr[i]%2==0 || arr[i]==0)
-        {
-          c++;
-        }
-    }
-    if(c==n)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
+  int i,n,c=0;
+  scanf("%d",&n);
+  int arr[n];
+  for(i=0;i<n;i++)
+  {
+      scanf("%d",&arr[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+      if(arr[i]%2==0 || arr[i]==0)
+      {
+      c++;
+      }
+  }
+  if(c==n)
+  {
+      printf("True");
+  }
+  else
+  {
+      printf("False");
+  }
 }
